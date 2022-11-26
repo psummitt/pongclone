0a629b13333a8a1057299b51735c3fa6bdb728c7
Paul M. Summitt <psummitt@gmail.com>
2022-11-25 20:55:08 -0600

    initial commit

---
+++ js/index.js
@@ -0,0 +1,40 @@
+const gameBoard = document.querySelector("#gameBoard");
+const ctx = gameBoard.getContext("2d");
+const scoreText = document.querySelector("#scoreText");
+const resetBtn = document.querySelector("#resetBtn");
+const gameWidth = gameBoard.width;
+const gameHeight = gameBoard.height;
+const boardBackground = "forestgreen";
+const paddle1Color = "lightblue";
+const paddle2Color = "red";
+const paddleBorder = "black"
+const ballColor = "yellow";
+const ballBorderColor = "black";
+const ballRadius = 12.5;
+const paddleSpeed = 50;
+let intervalID;
+let ballSpeed = 1;
+let ballX = gameWidth/2;
+let ballY = gameHeight/2;
+let ballXDirection = 0;
+let ballYDirection = 0;
+let player1Score = 0;
+let player2Score = 0;
+let paddle1 = {
+    width: 25,
+    height: 100,
+    x: 0,
+    y: 0
+}
+let paddle2 = {
+    width: 25,
+    height: 100,
+    x: gameWidth - 25,
+    y: gameHeight - 100
+}
+
+window.addEventListener("keydown", changeDirection);
+resetBtn.addEventListener("click", resetGame);
+
+gameStart();
+
