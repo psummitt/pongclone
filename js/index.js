bef128239ca136f94335875e7bbf828eeb301dd2
Paul M. Summitt <psummitt@gmail.com>
2022-11-26 19:36:15 -0600

    2nd commit

--- js/index.js
+++ js/index.js
@@ -38,3 +38,79 @@ resetBtn.addEventListener("click", resetGame);
 
 gameStart();
 
+function gameStart(){
+    createBall();
+    nextTick();
+};
+function nextTick(){
+    intervalID = setTimeout(() => {
+        clearBoard();
+        drawPaddles();
+        moveBall();
+        drawBall(ballX, ballY);
+        checkCollision();
+        nextTick;
+    }, 10)
+};
+function clearBoard(){
+    ctx.fillstyle = boardBackground;
+    ctx.fillRect(0, 0, gameWidth, gameHeight);
+};
+function drawPaddles(){
+    ctx.strokeStyle = paddleBorder;
+
+    ctx.fillstyle = paddle1Color;
+    ctx.fillRect(paddle1.x, paddle1.y, paddle1.width, paddle1.height);
+    ctx.strokeRect(paddle1.x, paddle1.y, paddle1.width, paddle1.height);
+
+    ctx.fillstyle = paddle2Color;
+    ctx.fillRect(paddle2.x, paddle2.y, paddle2.width, paddle2.height);
+    ctx.strokeRect(paddle2.x, paddle2.y, paddle2.width, paddle2.height);
+
+};
+function createBall(){};;
+function moveBall(){};
+function drawBall(ballX, ballY){
+    ctx.fillStyle - ballColor;
+    ctx.strokeStyle = ballBorderColor;
+    ctx.lineWidth = 2;
+    ctx.biginPath();
+    ctx.arc(ballX, ballY, ballRadius, 0, 2 * Math.PI);
+    ctx.stroke();
+    ctx.fill();
+};
+function checkCollision(){};
+function changeDirection(event){
+    const keyPressed = event.keyCode;
+    const paddle1Up = 87;
+    const paddle1Down = 83;
+    const paddle2Up = 38;
+    const paddle2Down = 40;
+
+    switch(keyPressed){
+        case(paddle1Up):
+            if(paddle1.y > 0){
+                paddle1.y -= paddleSpeed;
+            }
+            break;
+        case(paddle1Down):
+            if(paddle1.y < gameHeight - paddle1.height){
+                paddle1.y += paddleSpeed;
+            }
+            break;
+        case(paddle2Up):
+            if(paddle2.y > 0){
+            paddle2.y -= paddleSpeed;
+            }
+            break;
+        case(paddle2Down):
+            if(paddle2.y < gameHeight - paddle2.height){
+            paddle2.y += paddleSpeed;
+            }
+            break;
+    }
+
+};
+function updateScore(){}
+function resetGame(){};
+
